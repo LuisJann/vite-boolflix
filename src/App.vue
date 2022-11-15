c6e0b88357af3a9aba38f5b99c6086c789066810
LuisJann <giannoneluigi10@gmail.com>
2022-11-15 16:59:10 +0100

    add rate star 6 flag

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import SearchApp from './components/SearchApp.vue';
 import AppList from './components/AppList.vue';
 
+
+
 import {store} from "./store";
 import axios from "axios";
 export default{
@@ -14,28 +16,27 @@ export default{
       store,
     }
   },
-  created(){
-    this.titleFilter()
-
-  },
   methods: {
     titleFilter(){
-      let apiUrl = `${store.apiMovieURL}?api_key=${store.apiKey}`
+      let apiUrlMovie = `${store.apiMovieURL}?api_key=${store.apiKey}`
       let apiUrlSeries = `${store.apiSeriesURL}?api_key=${store.apiKey}`
       if(this.store.searchKey !== ""){
-        apiUrl += `&query=${this.store.searchKey}`
+        apiUrlMovie += `&query=${this.store.searchKey}`
         apiUrlSeries += `&query=${this.store.searchKey}`
       }
-      axios.get(apiUrl).then((resp) => {
+      axios.get(apiUrlMovie).then((resp) => {
         this.store.movies = resp.data.results
         console.log(this.store.movies);
+      }).catch(err =>{
+        console.log(err);
       })
       axios.get(apiUrlSeries).then((resp) => {
         this.store.series = resp.data.results 
         console.log(this.store.series);
+      }).catch(err =>{
+        console.log(err);
       })
-      
-      
+      this.store.searchKey="";
     },
   }
 }
@@ -46,6 +47,7 @@ export default{
   <AppList />
 </template>
 
-<style>
+<style lang="scss">
+@use "./styles/general.scss" as*;
 
 </style>
--- src/components/AppList.vue
+++ src/components/AppList.vue
@@ -5,30 +5,42 @@ export default {
     name:"AppList",
     data(){
         return{
-            store
+            store,
+            nationFlag:[`en`, `it`, `es`]
         }
+    },
+    methods:{
+        getImg(imgName){
+            return new URL (`../assets/images/${imgName}.png`, import.meta.url).href;
+        },
     },
 }
 </script>
 
 <template>
-    <h2>Film</h2>
+    <h2>Movies</h2>
     <div class="section">
         <div class="card" v-for="(movie,index) in store.movies" :key="index">
             <h2>{{movie.title}}</h2>
             <h3>{{movie.original_title}}</h3>
-            <p>{{movie.original_language}}</p>
-            <span>{{movie.vote_average}}</span>
-        </div>
+            <div>
+                <img v-if="nationFlag.includes(movie.original_language)" :src="getImg(movie.original_language)" alt="">
+                <p v-else>{{movie.original_language}}</p>
+            </div>
+                <span><i v-for="number in Math.ceil(movie.vote_average / 2)" class="fa-solid fa-star"></i></span>
+            </div>
     </div>
     <h2>Series</h2>
     <div class="section">
         <div class="card" v-for="(series,index) in store.series" :key="index">
             <h2>{{series.name}}</h2>
             <h3>{{series.original_name}}</h3>
-            <p>{{series.original_language}}</p>
-            <span>{{series.vote_average}}</span>
-        </div>
+            <div>
+                <img v-if="nationFlag.includes(series.original_language)" :src="getImg(series.original_language)" alt="">
+                <p v-else>{{series.original_language}}</p>
+            </div>
+                <span><i v-for="number in Math.ceil(series.vote_average / 2)" class="fa-solid fa-star"></i></span>
+            </div>
     </div>
 </template>
 
@@ -37,10 +49,18 @@ export default {
     display: flex;
     flex-wrap: wrap;
     padding: 10px;
+    width: 90%;
+    margin: 0 auto;
 }
 
 .card{
     border: 1px solid black;
     padding: 10px;
+    width: calc(100% / 5);
+}
+
+img{
+    width: 30px;
+    height: 30px;
 }
 </style>
